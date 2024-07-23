00f48c3dad06daab1550912bd459ef260e70ba33
DIgnacioAlvarez <alvarez.danign@gmail.com>
2024-07-23 17:56:47 -0300

    Se eliminaron archivos innecesario. Se instalo react-router-dom. Se modifico index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+)
 reportWebVitals(sendToVercelAnalytics);

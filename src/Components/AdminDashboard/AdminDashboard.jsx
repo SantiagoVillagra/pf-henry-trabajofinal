fa625fd66fc32edc8dae701e99ab47706279e477
SantiagoVillagra <sanmavillagra@gmail.com>
2024-07-31 16:55:22 -0300

    cree panel de tablas en adminDashboard

--- src/Components/AdminDashboard/AdminDashboard.jsx
+++ src/Components/AdminDashboard/AdminDashboard.jsx
@@ -2,21 +2,36 @@ import { logoutUser } from "../../Redux/Actions"
 import { useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import Create from "../Create/Create"
+import { TabView, TabPanel } from 'primereact/tabview';
 
 export default function AdminDashboard() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const logOut = () => {
+    dispatch(logoutUser());
+    navigate("/home");
+  };
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const logOut = () => {
-        dispatch(logoutUser())
-        navigate("/home")
-    }
+  return (
+    <div>
+      <TabView>
+        <TabPanel header="Carga nuevas zapatillas">
+          <Create></Create>
+        </TabPanel>
+        <TabPanel header="Elimina/oculta productos">
+          <p className="m-0">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+            eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci
+            velit, sed quia non numquam eius modi.
+          </p>
+        </TabPanel>
+      </TabView>
 
-    return (
-        <div>
-            <Create></Create>
-            <p>Soy un AdminDashboard</p>
-            <button onClick={() => logOut()}>LOG OUT</button>
-        </div>
-    )
+      <button onClick={() => logOut()}>LOG OUT</button>
+    </div>
+  );
 }

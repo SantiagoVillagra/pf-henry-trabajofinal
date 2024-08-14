08222305d67c47f99c097fab53816f6b67fbb4c9
DIgnacioAlvarez <alvarez.danign@gmail.com>
2024-08-14 17:15:46 -0300

    Finalizado Signup

--- src/mockDB/mockSignUp.js
+++ src/mockDB/mockSignUp.js
@@ -1,107 +1,43 @@
 import users from "./mockUserData"
-import Swal from "sweetalert2";
+import alertSwal from "../funcs/alertSwal"
+import axios from "axios"
 
-export default function signUp (userData, errors) {
+export default async function signUp (userData, errors) {
 
     const { email, password, passwordRepeat, username } = userData
 
     if (!email || !password || !passwordRepeat || !username) {
-        //window.alert("Faltan campos por completar")
-        Swal.fire({
-            title: "Faltan campos por completar",
-            showClass: {
-                popup: `
-                    animate__animated
-                    animate__fadeInUp
-                    animate__faster
-                `
-            },
-            hideClass: {
-                popup: `
-                    animate__animated
-                    animate__fadeOutDown
-                    animate__faster
-                `
-            }
-        });
+        alertSwal("Faltan campos por completar")
         return
     }
-
-    const existentUser = users.find(user => user.email === email)
-
-    if (existentUser) {
-        //window.alert("Este email corresponde a un usuario ya registrado")
-        Swal.fire({
-            title: "Este email corresponde a un usuario ya registrado",
-            showClass: {
-                popup: `
-                    animate__animated
-                    animate__fadeInUp
-                    animate__faster
-                `
-            },
-            hideClass: {
-                popup: `
-                    animate__animated
-                    animate__fadeOutDown
-                    animate__faster
-                `
-            }
-        });
-        return
-    }
-
+    
     const arrayErrors = Object.values(errors)
     const thereAreErrors = arrayErrors.find((error) => error !== "")
 
     if (thereAreErrors) {
-        //window.alert("Completa los campos según las condiciones especificadas")
-        Swal.fire({
-            title: "Completa los campos según las condiciones especificadas",
-            showClass: {
-                popup: `
-                    animate__animated
-                    animate__fadeInUp
-                    animate__faster
-                `
-            },
-            hideClass: {
-                popup: `
-                    animate__animated
-                    animate__fadeOutDown
-                    animate__faster
-                `
-            }
-        });
+        alertSwal("Completa los campos según las condiciones especificadas")
         return
     }
-    
 
-    users.push({
-        username: username,
-        email: email,
-        password: password,
-        isadmin: false
-    })
-    console.log(users);
-    //window.alert("Registro exitoso")
-    Swal.fire({
-        title: "Registro exitoso",
-        showClass: {
-            popup: `
-                animate__animated
-                animate__fadeInUp
-                animate__faster
-            `
-        },
-        hideClass: {
-            popup: `
-                animate__animated
-                animate__fadeOutDown
-                animate__faster
-            `
+    const body = {username, email, password, isAdmin: false}
+
+    try {
+        var signupUser = await axios.post(`https://e-commerse-fc.onrender.com/api/auth/register`, body)
+        console.log(signupUser)
+    } catch ({response: {data}}) {
+        const {message} = data
+
+        if (message === "Already registered email") {
+            alertSwal("Este email ya está registrado")
         }
-    });
+
+        return
+    }
+
+    console.log(signupUser);
+    
+    alertSwal("Registro exitoso")
 
     return ("flag")
 }
+

3d34c484a1dcce68f3b3a7ccf2d6b12137a64e3f
SantiagoVillagra <sanmavillagra@gmail.com>
2024-08-23 17:56:29 -0300

    push socket

--- src/Components/Socket/Socket.js
+++ src/Components/Socket/Socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 const socket = io('https://pf-henry-trabajofinal.vercel.app/', {
   withCredentials: true, // Esto permite el uso de cookies de sesión si es necesario
-});
+});;
 
 
 export default socket;
